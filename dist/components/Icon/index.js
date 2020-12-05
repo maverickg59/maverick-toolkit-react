c29acbe06f1153bfe52e86a27921a9a124cefbaa
Christopher White <christopher.evan.white88@gmail.com>
2020-12-05 01:12:57 -0700

    built with babel and bumped version again to publish

--- dist/components/Icon/index.js
+++ dist/components/Icon/index.js
@@ -1,27 +1,23 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
-});
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.Button;
-  }
 });
 Object.defineProperty(exports, "Icon", {
   enumerable: true,
   get: function get() {
-    return _Icon.Icon;
+    return _Icon.default;
   }
 });
 Object.defineProperty(exports, "iconLibrary", {
   enumerable: true,
   get: function get() {
-    return _Icon.iconLibrary;
+    return _library.default;
   }
 });
 
-var _Button = require("./components/Button");
+var _Icon = _interopRequireDefault(require("./Icon"));
 
-var _Icon = require("./components/Icon");
+var _library = _interopRequireDefault(require("./library"));
