091a2a60979fb35e6562ea76d582ae2a8f26ef37
Christopher White <christopher.evan.white88@gmail.com>
2020-12-05 23:00:53 -0700

    created input component and updated classes pattern in other components

--- src/lib/components/Icon/Icon.js
+++ src/lib/components/Icon/Icon.js
@@ -3,10 +3,11 @@ import cn from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Icon({ className, size, color, icon }) {
+  const classes = cn(className, 'c-icon')
   return (
     <span>
       <FontAwesomeIcon
-        className={cn(className, 'c-icon')}
+        className={classes}
         size={size}
         color={color}
         icon={icon}
