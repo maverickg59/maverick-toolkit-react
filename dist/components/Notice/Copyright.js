c7ae634cdedd8a8cf156efc086d3f952fee242e0
Christopher White <christopher.evan.white88@gmail.com>
2020-12-16 19:18:17 -0700

    build to update dist

--- dist/components/Notice/Copyright.js
+++ dist/components/Notice/Copyright.js
@@ -9,15 +9,25 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _classnames = _interopRequireDefault(require("classnames"));
+
 var Copyright = function Copyright(_ref) {
-  var established = _ref.established,
+  var className = _ref.className,
+      established = _ref.established,
       entity = _ref.entity,
       msg = _ref.msg;
+  var classes = (0, _classnames.default)(className, 'c-copyright__notice');
   var date = new Date();
   var year = date.getFullYear();
   var text = "\xA9 ".concat(established === parseInt(year) ? '' : established + ' - ').concat(year, " ").concat(entity).concat(msg ? ' ' + msg + '.' : '.', " All rights reserved.");
-  return /*#__PURE__*/_react.default.createElement("p", null, text);
+  return /*#__PURE__*/_react.default.createElement("p", {
+    className: classes
+  }, text);
 };
 
+Copyright.defaultProps = {
+  className: '',
+  msg: ''
+};
 var _default = Copyright;
 exports.default = _default;
