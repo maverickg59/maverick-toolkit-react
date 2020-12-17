e4042d7904a6a218b19eab79a04d7c3f3f34a28b
Christopher White <christopher.evan.white88@gmail.com>
2020-12-16 19:15:16 -0700

    passed className to copyright notice

--- src/lib/components/Notice/Copyright.js
+++ src/lib/components/Notice/Copyright.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
-const Copyright = ({ established, entity, msg }) => {
+const Copyright = ({ className, established, entity, msg }) => {
+  const classes = cn(className, 'c-copyright__notice')
   const date = new Date()
   const year = date.getFullYear()
   const text = `${'\u00A9 '}${
     established === parseInt(year) ? '' : established + ' - '
   }${year} ${entity}${msg ? ' ' + msg + '.' : '.'} All rights reserved.`
-  return <p>{text}</p>
+  return <p className={classes}>{text}</p>
 }
 
 Copyright.propTypes = {
+  className: PropTypes.string,
   established: PropTypes.number.isRequired,
   entity: PropTypes.string.isRequired,
+  msg: PropTypes.string,
+}
+
+Copyright.defaultProps = {
+  className: '',
+  msg: '',
 }
 
 export default Copyright
